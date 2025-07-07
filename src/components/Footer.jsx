4ef74b91e5e297e99ed3a50f7b7063706d59084a
Arihant-2004 <156528385+Arihant-2004@users.noreply.github.com>
2025-07-07 11:22:35 +0530

    finalcommit
    ui changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,30 @@
+import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Footer = () => {
+  
   return (
     <div className="md:mx-10">
     <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm">
       <div>
         <img className="mb-5 w-40" src={assets.logo} alt="" />
         <p className="w-full md:w-2/3 text-gray-600 leading-6">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.
         </p>
       </div>
       <div>
         <p className="text-xl font-medium mb-5">COMPANY</p>
         <ul className="flex flex-col gap-2 text-gray-600">
-          <li>Home</li>
-          <li>About us</li>
-          <li>Delivery</li>
-          <li>Privacy policy</li>
+         <Link to="/home">Home</Link>
+          <Link to="/about">About us</Link>
+          <Link to="/contact">Contact</Link>
         </ul>
       </div>
       <div>
         <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
         <ul className="flex flex-col gap-2 text-gray-600">
-          <li>+0-000-000-000</li>
-          <li>greatstackdev@gmail.com</li>
+          <li>+91-96013334</li>
+          <li>arihant@gmail.com</li>
         </ul>
       </div>
     
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,20 +19,13 @@ export const Contact = () => {
         <div className="flex flex-col justify-center items-start gap-6">
           <p className=" font-semibold text-lg text-gray-600">OUR OFFICE</p>
           <p className=" text-gray-500">
-            00000 Willms Station <br /> Suite 000, Washington, USA
+            Willms Station <br /> Suite  Washington, USA
           </p>
           <p className=" text-gray-500">
-            Tel: (000) 000-0000 <br /> Email: greatstackdev@gmail.com
+            Tel:  0151-20250 <br /> Email: arihant@gmail.com
           </p>
-          <p className=" font-semibold text-lg text-gray-600">
-            CAREERS AT PRESCRIPTO
-          </p>
-          <p className=" text-gray-500">
-            Learn more about our teams and job openings.
-          </p>
-          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
-            Explore Jobs
-          </button>
+        
+          
         </div>
       </div>
     </div>
